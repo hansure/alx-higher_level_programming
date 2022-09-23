1a94ea663c4a27ccedf19e950e1a2327d5b25c70
Surafel Melese <surafelmelese123@gmail.com>
2022-09-23 09:46:38 -0700

    First commit

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+      a: First integer
+      b: Second integer
+
+    Returns:
+       The return value. a + b
+       """
+    return (a + b)
