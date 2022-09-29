ac4209d69e3549d517aea98a794ac5033cd0dcd4
Surafel Melese <surafelmelese123@gmail.com>
2022-09-29 10:28:51 -0700

    Commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_dlistint: a function that prints the number of node
+ * @h: a pointer points toa list
+ * Return:  Number of node
+ **/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+
+	const dlistint_t *node = h;
+	const count = 0;
+
+	while(node)
+	{
+	 
+		printf("%i\n", node->n);
+		count++;
+		node = node->next;
+	}
+
+	return (count);
+}
+
